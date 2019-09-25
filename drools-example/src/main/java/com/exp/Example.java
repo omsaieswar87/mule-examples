1d2d6e47190e5507edb9a1496308eae8a24b2196
Mosip2 <Mosip2@b2md22167.mindtree.com>
2019-09-25 12:48:20 +0530

    Added the examples

---
+++ drools-example/src/main/java/com/exp/Example.java
@@ -0,0 +1,32 @@
+package com.exp;
+
+public class Example {
+	private String name;
+	private String address;
+	private String salary;
+	private String status;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
---
+++ drools-example/src/main/java/com/exp/ExampleCollection.java
@@ -0,0 +1,16 @@
+package com.exp;
+
+import java.util.List;
+
+public class ExampleCollection {
+
+	public List<Example> listExamples;
+
+	public List<Example> getListExamples() {
+		return listExamples;
+	}
+
+	public void setListExamples(List<Example> listExamples) {
+		this.listExamples = listExamples;
+	}
+}
